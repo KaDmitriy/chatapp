5cb1c87b127fcce28868fbf9649e3e97bfe68a53
KaDmitriy <ka.dmitriy@gmail.com>
2025-07-31 18:43:35 +0300

    clean code

--- chatapp-server/src/main/java/kda/chatapp/server/controller/Chat.java
+++ chatapp-server/src/main/java/kda/chatapp/server/controller/Chat.java
@@ -2,17 +2,12 @@ package kda.chatapp.server.controller;
 
 import kda.chatapp.server.dto.Message;
 import kda.chatapp.server.model.Greeting;
-import kda.chatapp.server.model.HelloMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
-
-import java.util.UUID;
 
 @Controller
 public class Chat {
@@ -22,7 +17,7 @@ public class Chat {
     @MessageMapping("/send")
     @SendToUser("/topic/to")
     public Greeting greeting(Message message) throws Exception {
-        log.info("hello Chat. Message text:"+message.getText()+" chatUID:"+message.getChatUID().toString());
+        log.info("hello Chat. Message text:{} chatUID:{}", message.getText(), message.getChatUID().toString());
         Thread.sleep(1000); // simulated delay
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getText()) + "!");
     }
